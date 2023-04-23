86acbb5014a2f28d2e2f1442f6f1591b2924a5c9
Diovane Pacheti <negociospacheti@gmail.com>
2023-04-23 15:37:15 -0300

    :recycle: refactor code clear with components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,13 @@
+import { Footer } from '@/components/footer/Footer';
+import { Sidebar } from '@/components/sidebar/Sidebar';
 import { Home as HomeIcon, Search,  Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Volume, Maximize, Laptop2} from 'lucide-react';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    /*h-screen: fara com que a div ocupe toda a altura da pagina 
-      flex: permitira que a div seja redimencionada 
-      flex-1: dimenciona o restante do espaço que sobrar 
-    */
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-72 bg-zinc-950 p-6">
-          <div className='flex items-center gap-2'>
-            <div className='w-3 h-3 bg-red-500 rounded-full'/>
-            <div className='w-3 h-3 bg-yellow-500 rounded-full'/>
-            <div className='w-3 h-3 bg-green-500 rounded-full'/>
-          </div>
-          <nav className='space-y-5 mt-10'>
-            <a href="" className=" flex items-center gap-3 text-sm font-semibold text-zinc-200"><HomeIcon />Home</a>
-            <a href="" className=" flex items-center gap-3 text-sm font-semibold text-zinc-200"><Search />Search</a>
-            <a href="" className=" flex items-center gap-3 text-sm font-semibold text-zinc-200"><Library />Your Library</a>
-          </nav>
-          <nav className='mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3'>
-            <a href="" className="text-sm text-zinc-400">Hot Hits Brasil</a>
-            <a href="" className="text-sm text-zinc-400">Aniver Funk</a>
-            <a href="" className="text-sm text-zinc-400">My Playlist #13</a>
-            <a href="" className="text-sm text-zinc-400">Top Brasil</a>
-          </nav>
-        </aside>
+        <Sidebar />
         <main className="flex-1 p-6" > 
           <div className="flex items-center gap-4">
             <button className="rounded-full bg-black/40 p-1"><ChevronLeft /></button>
@@ -72,45 +53,7 @@ export default function Home() {
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
-        <div className="flex items-center gap-3">
-            <Image className="w-full" src="/album3.jpg" width={48} height={48} alt="capado album"/>
-            <div className="flex flex-col">
-              <strong className="font-normal">Black Hole Sun</strong>
-              <span className="text-xs text-zinc-400">Soundgarden</span>
-            </div>
-        </div>
-        <div className='flex flex-col items-center gap-2'>
-          <div className="flex items-center gap-6">
-            <Shuffle size={20} className="text-zinc-500"/>
-            <SkipBack size={20} className="text-zinc-500"/>
-            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
-              <Play />
-            </button>
-            <SkipForward size={20} className="text-zinc-500"/>
-            <Repeat size={20} className="text-zinc-500"/>
-          </div>  
-          <div className="flex items-center gap-2">
-            <span className="text-xs text-zinc-400">0:31</span>
-            <div className="h-1 rounded-full w-96 bg-zinc-600">
-              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>  
-            </div>
-            <span className="text-xs text-zinc-400">2:15</span>
-          </div>
-        </div>
-        <div className="flex items-center gap-4">
-          <Mic2 size={20} />
-          <LayoutList size={20} />
-          <Laptop2 size={20} />
-          <div className="flex items-center gap-2">
-            <Volume size={20} />
-            <div className="h-1 rounded-full w-24 bg-zinc-600">
-              <div className="bg-zinc-200 w-10 h-1 rounded-full"></div>  
-            </div>
-          </div>
-          <Maximize size={20} />
-        </div>
-      </footer>
+      <Footer />
     </div>
   )
 }

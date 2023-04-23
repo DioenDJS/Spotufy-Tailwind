e18eab7992bd6aa7e01117401438ec96cddb66fc
Diovane Pacheti <negociospacheti@gmail.com>
2023-04-23 15:03:05 -0300

    :sparkles: create main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,116 @@
+import { Home as HomeIcon, Search,  Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Volume, Maximize, Laptop2} from 'lucide-react';
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <main></main>
+    /*h-screen: fara com que a div ocupe toda a altura da pagina 
+      flex: permitira que a div seja redimencionada 
+      flex-1: dimenciona o restante do espaço que sobrar 
+    */
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-72 bg-zinc-950 p-6">
+          <div className='flex items-center gap-2'>
+            <div className='w-3 h-3 bg-red-500 rounded-full'/>
+            <div className='w-3 h-3 bg-yellow-500 rounded-full'/>
+            <div className='w-3 h-3 bg-green-500 rounded-full'/>
+          </div>
+          <nav className='space-y-5 mt-10'>
+            <a href="" className=" flex items-center gap-3 text-sm font-semibold text-zinc-200"><HomeIcon />Home</a>
+            <a href="" className=" flex items-center gap-3 text-sm font-semibold text-zinc-200"><Search />Search</a>
+            <a href="" className=" flex items-center gap-3 text-sm font-semibold text-zinc-200"><Library />Your Library</a>
+          </nav>
+          <nav className='mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3'>
+            <a href="" className="text-sm text-zinc-400">Hot Hits Brasil</a>
+            <a href="" className="text-sm text-zinc-400">Aniver Funk</a>
+            <a href="" className="text-sm text-zinc-400">My Playlist #13</a>
+            <a href="" className="text-sm text-zinc-400">Top Brasil</a>
+          </nav>
+        </aside>
+        <main className="flex-1 p-6" > 
+          <div className="flex items-center gap-4">
+            <button className="rounded-full bg-black/40 p-1"><ChevronLeft /></button>
+            <button className="rounded-full bg-black/40 p-1"><ChevronRight /></button>
+          </div>
+          <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"><Image src="/album.jpg" width={104} height={104} alt="capado album"/><strong>Album</strong><button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible"><Play /></button></a>
+            <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"><Image src="/album.jpg" width={104} height={104} alt="capado album"/><strong>Album</strong><button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible"><Play /></button></a>
+            <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"><Image src="/album.jpg" width={104} height={104} alt="capado album"/><strong>Album</strong><button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible"><Play /></button></a>
+            <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"><Image src="/album.jpg" width={104} height={104} alt="capado album"/><strong>Album</strong><button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible"><Play /></button></a>
+            <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"><Image src="/album.jpg" width={104} height={104} alt="capado album"/><strong>Album</strong><button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible"><Play /></button></a>
+            <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"><Image src="/album.jpg" width={104} height={104} alt="capado album"/><strong>Album</strong><button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible"><Play /></button></a>
+          </div>
+
+          <h2 className='font-semibold text-2xl mt-10'>Made for Dioendtsx</h2>
+          <div className="grid grid-cols-5 gap-4 mr-4">
+            <a href='#' className="flex flex-col gap-2 bg-white/5 p-3 rounded-md hover:bg-white/10">
+              <Image className="w-full" src="/album2.jpg" width={104} height={104} alt="capado album"/>
+              <strong className="font-semibold">Daile Mix 1</strong>
+              <span className="text-sm text-zinc-400">Walloms, COIN, girl in red and more.</span>
+            </a>
+            <a href='#' className="flex flex-col gap-2 bg-white/5 p-3 rounded-md hover:bg-white/10">
+              <Image className="w-full" src="/album2.jpg" width={104} height={104} alt="capado album"/>
+              <strong className="font-semibold">Daile Mix 1</strong>
+              <span className="text-sm text-zinc-400">Walloms, COIN, girl in red and more.</span>
+            </a>
+            <a href='#' className="flex flex-col gap-2 bg-white/5 p-3 rounded-md hover:bg-white/10">
+              <Image className="w-full" src="/album2.jpg" width={104} height={104} alt="capado album"/>
+              <strong className="font-semibold">Daile Mix 1</strong>
+              <span className="text-sm text-zinc-400">Walloms, COIN, girl in red and more.</span>
+            </a>
+            <a href='#' className="flex flex-col gap-2 bg-white/5 p-3 rounded-md hover:bg-white/10">
+              <Image className="w-full" src="/album2.jpg" width={104} height={104} alt="capado album"/>
+              <strong className="font-semibold">Daile Mix 1</strong>
+              <span className="text-sm text-zinc-400">Walloms, COIN, girl in red and more.</span>
+            </a>
+            <a href='#' className="flex flex-col gap-2 bg-white/5 p-3 rounded-md hover:bg-white/10">
+              <Image className="w-full" src="/album2.jpg" width={104} height={104} alt="capado album"/>
+              <strong className="font-semibold">Daile Mix 1</strong>
+              <span className="text-sm text-zinc-400">Walloms, COIN, girl in red and more.</span>
+            </a>
+          </div>
+        </main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
+        <div className="flex items-center gap-3">
+            <Image className="w-full" src="/album3.jpg" width={48} height={48} alt="capado album"/>
+            <div className="flex flex-col">
+              <strong className="font-normal">Black Hole Sun</strong>
+              <span className="text-xs text-zinc-400">Soundgarden</span>
+            </div>
+        </div>
+        <div className='flex flex-col items-center gap-2'>
+          <div className="flex items-center gap-6">
+            <Shuffle size={20} className="text-zinc-500"/>
+            <SkipBack size={20} className="text-zinc-500"/>
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
+              <Play />
+            </button>
+            <SkipForward size={20} className="text-zinc-500"/>
+            <Repeat size={20} className="text-zinc-500"/>
+          </div>  
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">0:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>  
+            </div>
+            <span className="text-xs text-zinc-400">2:15</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <Mic2 size={20} />
+          <LayoutList size={20} />
+          <Laptop2 size={20} />
+          <div className="flex items-center gap-2">
+            <Volume size={20} />
+            <div className="h-1 rounded-full w-24 bg-zinc-600">
+              <div className="bg-zinc-200 w-10 h-1 rounded-full"></div>  
+            </div>
+          </div>
+          <Maximize size={20} />
+        </div>
+      </footer>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,7 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      
-    },
+    extend: {},
   },
   plugins: [],
 }

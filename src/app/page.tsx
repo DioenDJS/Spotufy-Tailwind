fe3758d927fe3828971c98c47e56a47d02b065f4
Diovane Pacheti <negociospacheti@gmail.com>
2023-04-23 16:32:14 -0300

    :sparkles: create molecules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Footer } from '@/components/footer/Footer';
+import { PlayLists } from '@/components/molecules/playLists/PlayLists';
 import { Sidebar } from '@/components/sidebar/Sidebar';
 import { Home as HomeIcon, Search,  Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Volume, Maximize, Laptop2} from 'lucide-react';
 import Image from 'next/image';
@@ -14,15 +15,7 @@ export default function Home() {
             <button className="rounded-full bg-black/40 p-1"><ChevronRight /></button>
           </div>
           <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
-          <div className="grid grid-cols-3 gap-4 mt-4">
-            <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"><Image src="/album.jpg" width={104} height={104} alt="capado album"/><strong>Album</strong><button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible"><Play /></button></a>
-            <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"><Image src="/album.jpg" width={104} height={104} alt="capado album"/><strong>Album</strong><button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible"><Play /></button></a>
-            <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"><Image src="/album.jpg" width={104} height={104} alt="capado album"/><strong>Album</strong><button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible"><Play /></button></a>
-            <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"><Image src="/album.jpg" width={104} height={104} alt="capado album"/><strong>Album</strong><button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible"><Play /></button></a>
-            <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"><Image src="/album.jpg" width={104} height={104} alt="capado album"/><strong>Album</strong><button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible"><Play /></button></a>
-            <a href="#" className="bg-white/5 rounded group flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"><Image src="/album.jpg" width={104} height={104} alt="capado album"/><strong>Album</strong><button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible"><Play /></button></a>
-          </div>
-
+            <PlayLists />        
           <h2 className='font-semibold text-2xl mt-10'>Made for Dioendtsx</h2>
           <div className="grid grid-cols-5 gap-4 mr-4">
             <a href='#' className="flex flex-col gap-2 bg-white/5 p-3 rounded-md hover:bg-white/10">
